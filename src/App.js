3f7a9485b9a74e577989ff6b7302229fb7523f9b
DuminduHarshana <ruvinduharshana536@gmail.com>
2023-03-25 21:51:05 -0400

    all services updated

--- src/App.js
+++ src/App.js
@@ -2,36 +2,69 @@ import React, { Component } from 'react';
 
 import GitHubLogin from 'react-github-login';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: null,
+      error: null
+    };
+  }
 
+  onSuccessGithub = (response) => {
+    this.setState({
+      token: response.code,
+      error: null
+    });
+    this.sendTokenToEndpoint(response.code);
+  }
+
+  sendTokenToEndpoint = (token) => {
+    fetch('http://localhost:9090/githubLogin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        token: token
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Token sent successfully.');
+      } else {
+        throw new Error('Failed to send token.');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      this.setState({
+        token: null,
+        error: error.message
+      });
+    });
+  }
+
+ 
   render() {
-
-    const onSuccessGithub = (response) => {
-      console.log(response.code);
-    } 
-
     return (
       <div className="App" align="center">
-        <h1>LOGIN WITH GITHUB AND MICROSOFT</h1>
-
-          {/*CLIENTID REDIRECTURI NOT CREATED YET*/}
-
-          <GitHubLogin clientId="_"
-            onSuccess={onSuccessGithub}
+        <h1>LOGIN WITH GITHUB </h1>
+        {this.state.token ?
+          <p>Token: {this.state.token}</p>
+          :
+          <GitHubLogin clientId="00bba9c289b344fd4277"
+            onSuccess={this.onSuccessGithub}
             buttonText="LOGIN WITH GITHUB"
             className="git-login"
             valid={true}
-            redirectUri="_"
+            redirectUri="http://localhost:3000"
           />
-
-          <br />
-          <br />
-
-          
-
-        
-    
-
+        }
+        {this.state.error &&
+          <p>Error: {this.state.error}</p>
+        }
       </div>
     );
   }
